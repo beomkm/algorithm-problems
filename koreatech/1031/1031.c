4ffdc0977c2f1367fea54457fd9c8a77cde7a04e
Beom-yeong Kim <tibyte@naver.com>
2016-10-25 00:01:39 +0900

    Update 1031.c

--- koreatech/1031/1031.c
+++ koreatech/1031/1031.c
@@ -1,1 +1,1 @@
-main(p,q,i){char b[33];for(gets(&p);i=30,~scanf("%d%d",&p,&q);puts(b+i+1))for(;p;p/=q)b[i--]="0123456789ABCDEF"[p%q];}
+char b[32];main(p,q,i){for(gets(&p);i=30,~scanf("%d%d",&p,&q);puts(b+i+1))for(;p;p/=q)b[i--]="0123456789ABCDEF"[p%q];}
