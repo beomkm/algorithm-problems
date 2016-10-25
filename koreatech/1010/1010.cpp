f275c78bdee8cb6c6773f077b62c2f3a4a26785b
tibyte <ttl74283@gmail.com>
2016-10-26 00:47:30 +0900

    Add kut 1010

---
+++ koreatech/1010/1010.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+bool isPrime(int n)
+{
+	//omit 1
+	for (int i = 2; i*i <= n; i++) {
+		if (n%i == 0) return false;
+	}
+	return true;
+}
+
+void print(int n, int digit)
+{
+	if (digit==1) {
+		cout << n << endl;
+		return;
+	}
+	n *= 10;
+	for (int i = 1; i < 10; i += 2) {
+		if (i == 5) continue;
+		if (isPrime(n + i)) {
+			print(n + i, digit-1);
+		}
+	}
+}
+
+int main(void)
+{ 
+	int digit;
+	cin >> digit;
+
+	print(2, digit);
+	print(3, digit);
+	print(5, digit);
+	print(7, digit);
+	return 0;
+
+}
