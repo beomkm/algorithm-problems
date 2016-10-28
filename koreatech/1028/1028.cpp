221f4b9f6d9fabb90959156b2dc275f0f2ca7ebc
tibyte <ttl74283@gmail.com>
2016-10-28 19:56:05 +0900

    Create gitignore (include .exe)

---
+++ koreatech/1028/1028.cpp
@@ -0,0 +1,29 @@
+/*
+http://tibyte.kr/268
+*/
+
+#include <bits/stdc++.h>
+
+int main()
+{
+	int T;
+	int len, max;
+	char s[10001];
+	scanf("%d", &T);
+	
+	for(;T--;) {
+		scanf("%s", s);
+		len = max = 1;
+		int i;
+		for(i=0; s[i]; ++i) {
+			if(s[i]==s[i+1]) ++len;
+			else {
+				if(len>max) max=len;
+				len=1;
+			}
+		}
+		printf("%d\n", i-max);
+	}
+	
+	return 0;
+}
